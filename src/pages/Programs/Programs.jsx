58bb437f6d924e58f6cb1c649a10b31e1139718e
shohruhbek-dev <shohruhbek.abdusattarov.205@gmail.com>
2025-03-15 03:11:03 +0500

    translate 2

--- src/pages/Programs/Programs.jsx
+++ src/pages/Programs/Programs.jsx
@@ -3,6 +3,7 @@ import students2 from '../../images/students2.jpg'
 import { TypeAnimation } from "react-type-animation"
 import Aos from 'aos'
 import 'aos/dist/aos.css'
+import { useTranslation } from 'react-i18next'
 
 const Programs = () => {
 
@@ -30,6 +31,11 @@ const Programs = () => {
             window.scrollTo({ top: y, behavior: "smooth" });
         }
     };
+     const { t, i18n } = useTranslation()
+        const ChangeLng = (e) => {
+            const selectedLanguage = e.target.value
+            i18n.changeLanguage(selectedLanguage)
+        }
 
     return (
         <>
@@ -41,21 +47,19 @@ const Programs = () => {
                     backgroundSize: "cover",
                     backgroundPosition: "center"
                 }}>
-                <h1 className='main-text w-full max-w-[400px] text-xl md:max-w-[700px] md:text-3xl lg:max-w-[1000px] lg:text-4xl lg:px-5 lg:py-4 xl:text-5xl text-center text-white uppercase font-semibold px-4 py-3 rounded-2xl'>
-                    <TypeAnimation
-                        sequence={[
-                            "Universitetga tayyorlanish yili",
-                        ]}
-                        speed={50}
-                        cursor={false}
-                    />
+                <h1
+                data-aos='zoom-out-up' className='main-text w-full max-w-[400px] text-xl md:max-w-[700px] md:text-3xl lg:max-w-[1000px] lg:text-4xl lg:px-5 lg:py-4 xl:text-5xl text-center text-white uppercase font-semibold px-4 py-3 rounded-2xl'>
+                    
+                       
+                            {t("Universitetga tayyorlanish yili")}
+                    
                 </h1>
                 <a
                     onClick={handleScroll}
                     data-aos='zoom-out-up'
                     href='#form'
                     className='text-white bg-[#004D91] w-[240px] h-12 rounded-xl flex items-center justify-center '>
-                    <h4 className='text-[18px] mr-2'>Hozir murojaat qiling</h4>
+                    <h4 className='text-[18px] mr-2'>{t("Hozir murojaat qiling")}</h4>
                     <i className="fa-solid fa-down-long text-[18px] mt-1.5"></i>
                 </a>
             </div>
@@ -65,7 +69,7 @@ const Programs = () => {
             )}
 
             <section className='w-[90%] m-auto h-auto py-4 bg-[#004D91] mt-4 rounded-2xl'>
-                <h1 data-aos='zoom-in' className='text-2xl text-center font-semibold text-white px-2'>Akademik muvaffaqiyat uchun poydevor qo'yish: Universitetga o'tishdagi muhim qadam</h1>
+                <h1 data-aos='zoom-in' className='text-2xl text-center font-semibold text-white px-2'>{t("Akademik muvaffaqiyat uchun poydevor qo'yish: Universitetga o'tishdagi muhim qadam")}</h1>
             </section>
 
             <div className="main-content max-w-7xl w-full px-8 m-auto text-[#004D91]">
@@ -76,14 +80,14 @@ const Programs = () => {
                         className="bg-[#004D91] h-[2px] w-[100%] m-auto rounded-lg"></div>
                     <h1
                         data-aos="fade-right"
-                        className="text-2xl text-center md:text-3xl font-semibold uppercase">Turk tili ko'nikmalarini rivojlantirish va til ta'limi yordamlari:</h1>
+                        className="text-2xl text-center md:text-3xl font-semibold uppercase">{t("Turk tili ko'nikmalarini rivojlantirish va til ta'limi yordamlari:")}</h1>
                 </section>
 
                 <div className='flex max-[550px]:flex-col gap-6 flex-row justify-between mb-6'>
                     <h1
                         data-aos="fade-right"
                         className="mt-4 leading-relaxed font-medium lg:max-w-[700px] lg:text-[20px]">
-                        Ona tili turk tili bo'lmagan talabalar uchun turk tilini bilish Turkiyadagi universitetlarda tahsil olmoqchi bo'lganlar uchun juda muhimdir. Bu talabalarga til bilimlarini oshirishga yordam berish uchun turli turk tilini qoʻllab-quvvatlash dasturlari taklif etiladi. Bu dasturlar talabalarga turk tilidan akademik til sifatida samarali foydalanish imkonini berishni maqsad qilgan va odatda universitetlar tomonidan taqdim etiladi.
+                        {t("Ona tili turk tili bo'lmagan talabalar uchun turk tilini bilish Turkiyadagi universitetlarda tahsil olmoqchi bo'lganlar uchun juda muhimdir. Bu talabalarga til bilimlarini oshirishga yordam berish uchun turli turk tilini qoʻllab-quvvatlash dasturlari taklif etiladi. Bu dasturlar talabalarga turk tilidan akademik til sifatida samarali foydalanish imkonini berishni maqsad qilgan va odatda universitetlar tomonidan taqdim etiladi.")}
                     </h1>
                 </div>
 
@@ -105,14 +109,14 @@ const Programs = () => {
                         className="bg-[#004D91] h-[2px] w-[100%] m-auto rounded-lg"></div>
                     <h1
                         data-aos="zoom-in-up"
-                        className="text-2xl text-center md:text-3xl font-semibold uppercase">Akademik til imtihonlari:</h1>
+                        className="text-2xl text-center md:text-3xl font-semibold uppercase">{t("Akademik til imtihonlari:")}</h1>
                 </section>
 
                 <div className='flex max-[550px]:flex-col gap-6 flex-row justify-between'>
                     <h1
                         data-aos="zoom-in-up"
                         className="mt-4 leading-relaxed font-medium lg:max-w-[700px] lg:text-[20px]">
-                        Oliy ta’lim muassasalari qabul jarayonida talabalarning ta’lim tillarini bilish darajasini tekshirish uchun til bilish imtihonlarini tashkil qiladi. Ushbu imtihonlar talabalarning akademik matnlarni tushunishi va samarali muloqot qilishini baholaydi. Noto'g'ri deb topilgan talabalar til ko'nikmalarini oshirish uchun tayyorgarlik dasturlariga yo'naltiriladi.
+                       {t("Oliy ta’lim muassasalari qabul jarayonida talabalarning ta’lim tillarini bilish darajasini tekshirish uchun til bilish imtihonlarini tashkil qiladi. Ushbu imtihonlar talabalarning akademik matnlarni tushunishi va samarali muloqot qilishini baholaydi. Noto'g'ri deb topilgan talabalar til ko'nikmalarini oshirish uchun tayyorgarlik dasturlariga yo'naltiriladi.")}
                     </h1>
                 </div>
 
@@ -122,14 +126,14 @@ const Programs = () => {
                         className="bg-[#004D91] h-[2px] w-[100%] m-auto rounded-lg"></div>
                     <h1
                         data-aos="zoom-out"
-                        className="text-2xl text-center md:text-3xl font-semibold uppercase">Ko'p tilli ta'lim imkoniyatlari:</h1>
+                        className="text-2xl text-center md:text-3xl font-semibold uppercase">{t("Ko'p tilli ta'lim imkoniyatlari:")}</h1>
                 </section>
 
                 <div className='flex max-[550px]:flex-col gap-6 flex-row justify-between mb-4'>
                     <h1
                         data-aos="zoom-out"
                         className="mt-4 leading-relaxed font-medium lg:max-w-[700px] lg:text-[20px]">
-                        Turkiyadagi ta'lim muassasalari talabalarga ingliz, frantsuz va nemis kabi turli chet tillarida ta'lim olish imkoniyatini beradi. Bu imkoniyatlar talabalarga til koʻnikmalarini yaxshilaydigan va akademik muvaffaqiyatlarini qoʻllab-quvvatlovchi qoʻshimcha til kurslariga kirish imkonini beradi. Ushbu til o'qitish dasturlari odatda intensiv tarzda ishlab chiqilgan va bir o'quv yili davom etadi.
+                        {t("Turkiyadagi ta'lim muassasalari talabalarga ingliz, frantsuz va nemis kabi turli chet tillarida ta'lim olish imkoniyatini beradi. Bu imkoniyatlar talabalarga til koʻnikmalarini yaxshilaydigan va akademik muvaffaqiyatlarini qoʻllab-quvvatlovchi qoʻshimcha til kurslariga kirish imkonini beradi. Ushbu til o'qitish dasturlari odatda intensiv tarzda ishlab chiqilgan va bir o'quv yili davom etadi.")}
                     </h1>
                 </div>
 
@@ -149,18 +153,18 @@ const Programs = () => {
                         className="bg-[#004D91] h-[2px] w-[100%] m-auto rounded-lg"></div>
                     <h1
                         data-aos="zoom-out-up"
-                        className="text-2xl text-center md:text-3xl font-semibold uppercase">Til tayyorlash dasturlari:</h1>
+                        className="text-2xl text-center md:text-3xl font-semibold uppercase">{t("Til tayyorlash dasturlari:")}</h1>
                 </section>
 
                 <div className='flex max-[550px]:flex-col gap-6 flex-row justify-between'>
                     <h1
                         data-aos="zoom-out-up"
                         className="mt-4 leading-relaxed font-medium lg:max-w-[700px] lg:text-[20px]">
-                        Tilga tayyorgarlik dasturlari talabalarga akademik tillarda muvaffaqiyat qozonishlari uchun keng qamrovli treninglarni taklif qiladi. Bu dasturlar o‘quvchilarni darslarda faol ishtirok etishga, uy vazifalarini o‘z vaqtida bajarishga, taklif etilayotgan o‘quv materiallaridan unumli foydalanishga undaydi. Dastur oxirida talabalar keyingi ta'limni davom ettirish uchun zarur bo'lgan til bilimiga ega bo'lishlari taxmin qilinadi.
+                       {t("Tilga tayyorgarlik dasturlari talabalarga akademik tillarda muvaffaqiyat qozonishlari uchun keng qamrovli treninglarni taklif qiladi. Bu dasturlar o‘quvchilarni darslarda faol ishtirok etishga, uy vazifalarini o‘z vaqtida bajarishga, taklif etilayotgan o‘quv materiallaridan unumli foydalanishga undaydi. Dastur oxirida talabalar keyingi ta'limni davom ettirish uchun zarur bo'lgan til bilimiga ega bo'lishlari taxmin qilinadi.")}
                     </h1>
                 </div>
                 <h1 data-aos="zoom-out-up"
-                    className="mt-4 leading-relaxed font-medium lg:max-w-[700px] lg:text-[20px]">Turkiya xalqaro talabalarga turli tillarda ta’lim olish imkoniyatlarini taqdim etish orqali ta’lim sohasida global diqqat markaziga aylanishni maqsad qilgan. Turk tili asosiy ta'lim tili bo'lsa-da, universitetlar bir nechta til variantlari bilan global akademik tajribani taklif qilishadi.</h1>
+                    className="mt-4 leading-relaxed font-medium lg:max-w-[700px] lg:text-[20px]">{t("Turkiya xalqaro talabalarga turli tillarda ta’lim olish imkoniyatlarini taqdim etish orqali ta’lim sohasida global diqqat markaziga aylanishni maqsad qilgan. Turk tili asosiy ta'lim tili bo'lsa-da, universitetlar bir nechta til variantlari bilan global akademik tajribani taklif qilishadi.")}</h1>
 
             </div>
         </>
